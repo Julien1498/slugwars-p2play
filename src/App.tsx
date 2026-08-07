29373bb1d717f20d41b21b2814c78b7dfdaed63b
Julien1498 <tormento.jg@hotmail.fr>
2026-08-07 14:47:14 +0200

    fix(controls): forward targetPoint parameter in App.tsx onReleaseCharge callback to fix missing homing target

--- src/App.tsx
+++ src/App.tsx
@@ -122,7 +122,7 @@ export const App: React.FC<AppProps> = ({
         onStartSteer={(dir) => sendAction('START_STEER', { dir })}
         onStopSteer={() => sendAction('STOP_STEER')}
         onStartCharge={() => sendAction('START_CHARGE')}
-        onReleaseCharge={() => sendAction('RELEASE_CHARGE')}
+        onReleaseCharge={(targetPoint) => sendAction('RELEASE_CHARGE', { targetPoint })}
         onDetonate={() => sendAction('DETONATE')}
         onRestartGame={() => sendAction('RESTART_GAME')}
         onExit={onExit}
